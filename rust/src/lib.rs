46a13e3627aa513c9526cd9d476e15d34f90428c
sagecottrell <sage@cottrell.dev>
2026-09-23 16:43:22 -0700

    get player functioning

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,4 @@
-use godot::prelude::*;
+use godot::{classes::{Area3D, IArea3D}, prelude::*};
 
 mod player;
 
@@ -6,3 +6,46 @@ struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
+
+
+#[derive(GodotClass)]
+#[class(base=Node3D)]
+struct GLTFImport {
+    base: Base<Node3D>
+}
+
+
+#[godot_api]
+impl INode3D for GLTFImport {
+    fn init(base: Base<Node3D>) -> Self {
+        Self {base}
+    }
+}
+
+
+
+
+
+
+#[derive(GodotClass)]
+#[class(base=Area3D)]
+struct Platform {
+    base: Base<Area3D>
+}
+
+
+#[godot_api]
+impl IArea3D for Platform {
+    fn init(base: Base<Area3D>) -> Self {
+        Self {base}
+    }
+}
+
+
+#[godot_api]
+impl Platform {
+    #[func(virtual)]
+    fn get_anchor_transform(&self, _global_collision_point: Vector3) -> Transform3D {
+        self.base().get_global_transform()
+    }
+}
--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,44 +1,116 @@
-use godot::{classes::{ISprite2D, Sprite2D}, prelude::*};
+use godot::{
+    classes::{Input, RayCast3D, tween},
+    prelude::*,
+};
+
+use crate::{GLTFImport, Platform};
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=Node3D)]
 struct Player {
-    speed: f64,
-    angular_speed: f64,
+    base: Base<Node3D>,
 
-    base: Base<Sprite2D>
+    #[export]
+    time_to_move: f64,
+    #[export]
+    model: Option<Gd<GLTFImport>>,
+
+    // onready
+    raycast: Option<Gd<RayCast3D>>,
+
+    // runtime vars
+    up: Vector3,
+    start_transform: Transform3D,
+    target_transform: Transform3D,
+    moving: bool,
 }
 
 #[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-        
+impl INode3D for Player {
+    fn init(base: Base<Node3D>) -> Self {
         Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
+            time_to_move: 0.25,
+            model: None,
+            raycast: None,
+            up: Vector3::UP,
+            start_transform: Transform3D::IDENTITY,
+            target_transform: Transform3D::IDENTITY,
+            moving: false,
             base,
         }
     }
 
-    fn physics_process(&mut self, delta: f64) {
-        // GDScript code:
-        //
-        // rotation += angular_speed * delta
-        // var velocity = Vector2.UP.rotated(rotation) * speed
-        // position += velocity * delta
-        
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+    fn ready(&mut self) {
+        let raycast = self.base().try_get_node_as::<RayCast3D>("%RayCast3D");
+        self.raycast = raycast;
+    }
 
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        self.base_mut().translate(velocity * delta as f32);
-        
-        // or verbose: 
-        // let this = self.base_mut();
-        // this.set_position(
-        //     this.position() + velocity * delta as f32
-        // );
+    fn physics_process(&mut self, _delta: f64) {
+
+        if self.moving {
+            return;
+        }
+
+        let input = Input::singleton();
+        let direction = input.get_vector("left", "right", "forward", "backward");
+
+        if direction.is_zero_approx() {
+            return;
+        }
+
+        if let Some(mut model) = self.model.clone() {
+            let direction = Vector3::new(direction.x, 0., direction.y).normalized();
+            model.look_at(self.base().to_global(direction));
+        }
+
+        if let Some(raycast) = self.raycast.clone() {
+            let collider = raycast.get_collider();
+            if let Some(collider) = collider
+                && let Ok(platform) = collider.try_cast::<Platform>()
+            {
+                let target_transform = platform
+                    .bind()
+                    .get_anchor_transform(raycast.get_collision_point());
+                if target_transform.basis.tdoty(self.up) < 0.7 {
+                    return;
+                }
+
+                self.moving = true;
+                self.start_transform = self.base().get_global_transform();
+                self.target_transform = target_transform;
+                self.up = target_transform.basis.col_b();
+                let callable = self.base().callable("interpolate");
+                let time = self.time_to_move;
+                let tween = self
+                    .base_mut()
+                    .create_tween()
+                    .tween_method(&callable, &0.0.to_variant(), &1.0.to_variant(), time)
+                    .set_trans(tween::TransitionType::LINEAR);
+
+                tween
+                    .signals()
+                    .finished()
+                    .connect_other(self, Player::finish_interpolate);
+            }
+        }
+    }
+}
+
+#[godot_api]
+impl Player {
+    #[func]
+    fn interpolate(&mut self, weight: f32) {
+        let basis = self.start_transform.basis;
+        let origin = self.start_transform.origin;
+        let tbasis = self.target_transform.basis;
+        let torigin = self.target_transform.origin;
+        self.base_mut().set_global_transform(Transform3D {
+            basis: basis.slerp(&tbasis, weight),
+            origin: origin.slerp(torigin, weight),
+        });
+    }
+
+    fn finish_interpolate(&mut self) {
+        self.moving = false;
     }
 }
