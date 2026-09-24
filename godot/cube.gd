e9a6433f5ac4a4aa0fec6b0032aec4ace76ed60e
sagecottrell <sage@cottrell.dev>
2026-09-24 12:05:51 -0700

    fix rotation of player

---
+++ godot/cube.gd
@@ -0,0 +1,20 @@
+class_name Cube
+extends Platform
+
+@export var pos_x : Marker3D
+@export var neg_x : Marker3D
+@export var pos_y : Marker3D
+@export var neg_y : Marker3D
+@export var pos_z : Marker3D
+@export var neg_z : Marker3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _get_anchor_transform(global_collision_point: Vector3) -> Transform3D:
+	var local := to_local(global_collision_point)
+	var axes := [[neg_x, pos_x], [neg_y, pos_y], [neg_z, pos_z]]
+	var i := local.max_axis_index()
+	var b : Marker3D = axes[i][(sign(local[i]) + 1) / 2]
+	return b.global_transform
